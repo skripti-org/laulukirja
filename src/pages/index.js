a235560c3933adf0a84aa52c0a6fd05387bb2791
Juho Nykänen <juho.nykanen@productivityleap.com>
2022-09-17 19:04:58 +0300

    gatsbyn konffit

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const BlogIndex = ({ data, location }) => {
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
-          
+
           return (
             <li key={post.fields.slug}>
               <article
