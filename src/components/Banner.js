b1ba80437fc2cf760ed29dbf62de1208b8ccb24f
Santeri Mertakorpi <81715964+SanteriMertakorpi@users.noreply.github.com>
2023-10-16 22:00:52 +0300

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className="banner-wrapper">
       <div className="banner">
-        <h1 className="title">Vujusitsit</h1>
+        <h1 className="title">Uwusitsit</h1>
       </div>
     </div>
   )
