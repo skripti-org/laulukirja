ce11bb16691ee19d6659f0d3d2a0c130c987a598
Santeri Mertakorpi <81715964+SanteriMertakorpi@users.noreply.github.com>
2024-04-24 15:40:07 +0300

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
     <div className="banner-wrapper">
       <div className="banner">
         {/*Kirjoita title mielellään kokonaan pienellä, fontti on outo ja isot kirjaimet näyttää paskalta :D*/}
-        <h1 className="title">kaverisitsit</h1>
+        <h1 className="title">Vappusitsit</h1>
       </div>
     </div>
   )
